7f97aece85296d5d0059959762410a62c1d05469
Iván Canga <ivan.canga@corebiz.ag>
2020-08-19 13:53:21 -0300

    modules + firebase config + clean CRA

---
+++ src/containers/App/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+import firebase from "../../config/firebase";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+
+  return (
+    <div>Meetups creator</div>
+  )
+
+};
+
+export default App;
